a234b5bea9635875c46f8118c70f4e89c1ed57ae
Gabriel Mendes <ggml1@github.com>
2020-10-31 22:22:37 -0300

    Add plotting modules

--- ackley-function-evolutionary-strategy/main.py
+++ ackley-function-evolutionary-strategy/main.py
@@ -1,11 +1,11 @@
 from biology import *
-from utils import pop_best_fitness, pop_worst_fitness, pop_avg_fitness, pop_std_fitness, pop_avg_mut_step
+from utils import get_population_statistics, plot_array
 import matplotlib.pyplot as plt
 import logging
 from datetime import datetime
 from parameters import params as CFG
 
-# logging.basicConfig(filename="evol_strategy_run_{}.log".format(datetime.now().strftime("%d-%m-%Y-%H-%M-%SS")), level=logging.INFO)
+logging.basicConfig(filename="evol_strategy_run_{}.log".format(datetime.now().strftime("%d-%m-%Y-%H-%M-%SS")), level=logging.INFO)
 
 def main(number_of_iterations = CFG["ITR_AMT"],
          population_size = CFG["POP_SIZE"],
@@ -16,19 +16,30 @@ def main(number_of_iterations = CFG["ITR_AMT"],
 
   population = generate_population(population_size)
 
+  avg_fitnesses = []
+  std_dev_fitnesses = []
+  avg_mutation_steps = []
+  std_dev_mutation_steps = []
+
   for iteration in range(number_of_iterations):
     #sca = plt.scatter(iteration, population_average_fitness(population), s=200, lw=0, c='red', alpha=0.5); plt.pause(0.05)
 
-    #print("Iteration: {} - AVG Fitness: {} - Best Fitness: {} - Worst Fitness {}".format(str(iteration), population_average_fitness(population), population_best_fitness(population), population_worst_fitness(population)))
-    iteration_statistics = "\"{}\": {{ \"best_fitness\":{},  \"worst_fitness\":{}, \"avg_fitness\":{}, \"std_dev_fitness\":{}, \"avg_mutation_step\":{} }}".format(str(iteration),
-                                                                                                                                                                        pop_best_fitness(population),
-                                                                                                                                                                        pop_worst_fitness(population),
-                                                                                                                                                                        pop_avg_fitness(population),
-                                                                                                                                                                        pop_std_fitness(population),
-                                                                                                                                                                        pop_avg_mut_step(population)
-                                                                                                                                                                        )
+    pop_statistics = get_population_statistics(population)
+
+    avg_fitnesses.append(pop_statistics['avg_fitness'])
+    std_dev_fitnesses.append(pop_statistics['std_dev_fitness'])
+    avg_mutation_steps.append(pop_statistics['avg_mutation_step'])
+    std_dev_mutation_steps.append(pop_statistics['std_dev_mutation_step'])
+
+    iteration_statistics = '"{}"'.format(str(iteration)) + \
+                           '"best_fitness": {}'.format(pop_statistics['best_fitness']) + \
+                           '"worst_fitness": {}'.format(pop_statistics['worst_fitness']) + \
+                           '"avg_fitness": {}'.format(pop_statistics['avg_fitness']) + \
+                           '"std_dev_fitness": {}'.format(pop_statistics['std_dev_fitness']) + \
+                           '"avg_mutation_step": {}'.format(pop_statistics['avg_mutation_step']) 
+
     print(iteration_statistics)
-    # logging.info(iteration_statistics)
+    logging.info(iteration_statistics)
 
     children = []
     for i in range(parent_couples_per_iteration):
@@ -41,6 +52,10 @@ def main(number_of_iterations = CFG["ITR_AMT"],
     population = kill_suckers(population, children, population_size)
   
   #plt.ioff(); plt.show()
+  plot_array(avg_fitnesses, title = 'Fitness Average')
+  plot_array(std_dev_fitnesses, title = 'Fitness Std Deviation')
+  plot_array(avg_mutation_steps, title = 'Mutation Step Average')
+  plot_array(std_dev_mutation_steps, title = 'Mutation Step Std Deviation')
 
   return
 
--- ackley-function-evolutionary-strategy/utils.py
+++ ackley-function-evolutionary-strategy/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+import matplotlib.pyplot as plt
 from parameters import params as CFG
 
 def generate_random_double_array(length, lower_bound = CFG["LWR_BND"], upper_bound = CFG["UPR_BND"]):
@@ -31,6 +32,14 @@ def pop_avg_mut_step(population):
     avg = np.average(list(map(lambda ind: np.average(ind.sigma), population)))
   return avg
 
+def pop_std_dev_mut_step(population):
+  std_dev = 0
+  if CFG["MUT_TYP"] == "UNCORRELATED_SINGLE":
+    std_dev = np.std(list(map(lambda ind: ind.sigma[0], population)))
+  elif CFG["MUT_TYP"] == "UNCORRELATED_MANY":
+    std_dev = np.std(list(map(lambda ind: np.std(ind.sigma), population)))
+  return std_dev
+
 def gen_random_permutation(length):
   return list(map(lambda index : index - 1, np.random.permutation(length)))
 
@@ -39,3 +48,28 @@ def generate_random_integer(lower_bound = 2, upper_bound = CFG["POP_SIZE"]):
 
 def choose_k_from_array(array, k):
   return random.sample(array, k)
+
+def get_population_statistics(population):
+  return {
+    'best_fitness': pop_best_fitness(population),
+    'worst_fitness': pop_worst_fitness(population),
+    'avg_fitness': pop_avg_fitness(population),
+    'std_dev_fitness': pop_std_fitness(population),
+    'avg_mutation_step': pop_avg_mut_step(population),
+    'std_dev_mutation_step': pop_std_dev_mut_step(population)
+  }
+
+def plot_array(y, title = 'Untitled Plot', x_label = None, y_label = None):
+  x = [ i + 1 for i in range(len(y)) ]
+
+  fig, ax = plt.subplots()
+  ax.plot(x, y)
+  ax.set_title(title)
+  
+  if (x_label):
+    ax.set_xlabel(x_label)
+  
+  if (y_label):
+    ax.set_ylabel(y_label)
+
+  fig.savefig(f'{title}.png')
